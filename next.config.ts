515b158315eca44d1982fd0258277e30f7f4c31e
Oscar <oskharm12@gmail.com>
2025-03-21 22:41:24 -0300

    resolvendo problemas

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      process.env.SUPABASE_URL?.replace(/^https?:\/\//, '') || 'qfpygaqyldmthqakmisq.supabase.co',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.SUPABASE_URL?.replace(/^https?:\/\//, '') || 'qfpygaqyldmthqakmisq.supabase.co',
+        pathname: '/storage/v1/object/public/tiviai-images/profile-pictures/**', // Ajustado para o caminho correto das imagens
+      },
     ],
   },
 };
 
 export default nextConfig;
-//$env:PORT=3000; npm run dev
