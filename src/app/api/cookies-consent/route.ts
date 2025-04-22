dce5933656d41dbca6e16d58d3520727ddac86e0
Oscar <oskharm12@gmail.com>
2025-04-22 18:47:35 -0300

    solved error in cookies consent

---
+++ src/app/api/cookies-consent/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+export function GET(req:Request){
+console.log("requisição get");
+}
+
+
+export function POST(req:Request){
+console.log('requisção post')
+}
