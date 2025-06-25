39804bc4d17da1325494febc5b099a9996cdf082
Oscar <oskharm12@gmail.com>
2025-06-24 22:15:47 -0300

    voltando ao runtime anterior

--- src/app/api/internal/insight/psicochat/route.ts
+++ src/app/api/internal/insight/psicochat/route.ts
@@ -10,11 +10,7 @@ import { generateRN } from "@/app/util/RN";
 import { generateTRT } from "@/app/util/TRT";
 
 
-//export const runtime = 'edge';
-//o que houve
-export const runtime = 'node';
-
-
+export const runtime = 'edge';
 
 
 const openai = new OpenAI({
