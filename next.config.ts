9e8b759d70d6f62630abb2e71e71bc74eef33b00
Oscar <oskharm12@gmail.com>
2025-09-04 01:15:40 -0300

    teste

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-/* import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
@@ -19,41 +19,5 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig; */
+export default nextConfig; 
 
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'qfpygaqyldmthqakmisq.supabase.co',
-        pathname: "/storage/v1/object/public/tiviai-images/**",
-      },
-    ],
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '10mb',
-    },
-  },
-  async redirects() {
-    return [
-      {
-        source: '/api/webhooks/pagarme',
-        destination: '/api/webhooks/pagarme', // na verdade não muda, só evita 308
-        permanent: false,
-      },
-    ];
-  },
-};
-
-export default nextConfig;
-
-
-
-
-
-//https://qfpygaqyldmthqakmisq.supabase.co/storage/v1/object/public/tiviai-images/capa-livro/1751847364797-ChatGPT-Image-29-de-jun.-de-2025-143421.png
-//https://         dmthqakmisq.supabase.co/storage/v1/object/public/capas/capa-livro/1751847364797-ChatGPT-Image-29-de-jun.-de-2025-143421.png
