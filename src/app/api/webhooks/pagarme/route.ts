ffd4b1023acd01a92ff02f817ffaec6f710e88e0
Oscar <oskharm12@gmail.com>
2025-09-03 23:34:30 -0300

    teste

--- src/app/api/webhooks/pagarme/route.ts
+++ src/app/api/webhooks/pagarme/route.ts
@@ -49,8 +49,7 @@ export async function POST(req: NextRequest) {
  */
 
 
-// app/api/webhooks/pagarme/route.ts
-import { NextRequest, NextResponse } from "next/server";
+
 
 export async function POST(req: NextRequest) {
   try {
