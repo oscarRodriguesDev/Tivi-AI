b852e799735019f98630c9857542ce1646c13c8d
Oscar <oskharm12@gmail.com>
2025-09-09 00:13:31 -0300

    test temp for save buy

--- src/app/(private-access)/(acess-psicologo)/credit/[id]/components/modal-recharg.tsx
+++ src/app/(private-access)/(acess-psicologo)/credit/[id]/components/modal-recharg.tsx
@@ -236,6 +236,9 @@ export default function PaymentModal({ isOpen, onClose, produto }: PaymentModalP
       // Extrair dados do PIX
       const pixPayload = lastTransaction.qr_code;
       const pixQrCodeUrl = lastTransaction.qr_code_url;
+      // Fazer um temporizador para dar tempo de salvar a compra no BD antes de redirecionar para a tela do PIX
+      // Aguarda 1 segundo antes de redirecionar (ajuste se necessário)
+      await new Promise((resolve) => setTimeout(resolve, 3000));
       router.push(`/credit/pix/${encodeURIComponent(lastTransaction.qr_code_url)}`);
       onClose(); // fecha modal apenas no sucesso
       return {
